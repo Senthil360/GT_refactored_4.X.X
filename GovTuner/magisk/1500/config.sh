778671c41b7f102d6c65dbe9741ab6f9a24764a2
F4uzan <f4uzan@this.is.f4>
2017-12-26 17:12:25 +0000

    config: Fix permission path
    * The init file has been moved; this causes the installation of the ZIP to fail at setting permission
    * Also remove the huge block of comments up top

--- GovTuner/magisk/1500/config.sh
+++ GovTuner/magisk/1500/config.sh
@@ -20,32 +20,11 @@ print_modname() {
   ui_print "@GreekDragon"
 }
 
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info about how Magic Mount works, and why you need this
-
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here, it will override the example above
-# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
-REPLACE="
-"
-
 REPLACE="
 "
 
 set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0644
-  set_perm_recursive $MODPATH/system/etc/GovTuner/ 0 0 0755 0644
   set_perm $MODPATH/system/bin/govtuner 0 0 0777 02000
-  set_perm $MODPATH/system/etc/GovTuner/init/00gt_init 0 0 0777 02000
+  set_perm $MODPATH/system/etc/GovTuner/00gt_init 0 0 0777 02000
 }
