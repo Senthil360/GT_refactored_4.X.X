90a077b8608ea037a89002546f3139f7ec195ea6
F4uzan <f4uzan@this.is.f4>
2018-05-03 15:54:57 +0100

    magisk: Set proper permission for GovTuner_hybrid

--- GovTuner/magisk/1500/config.sh
+++ GovTuner/magisk/1500/config.sh
@@ -28,4 +28,5 @@ set_permissions() {
   set_perm_recursive  $MODPATH/system/etc/GovTuner/ 0 0 0755 0644
   set_perm $MODPATH/system/bin/govtuner 0 0 0777
   set_perm $MODPATH/system/etc/GovTuner/00gt_init 0 0 0777
+  set_perm $MODPATH/system/etc/GovTuner/profiles/GovTuner_hybrid 0 0 0777
 }
