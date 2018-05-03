4291bac2b25f8afb40e68482f05e1441b933062b
F4uzan <f4uzan@this.is.f4>
2018-05-03 12:58:19 +0100

    build: Huge fixes for the new Windows script
    After further testing, and reviewing, the Windows script seems to be working fine. These changes are made to it in the process:
    * Fix string comparison breakage; use EQU and ! to properly address them.
    * Silence the output of xcopy, copy, and rd.
    * Fix xcopy's switches to unbreak file copy.
    * Use pushd instead of cd to go back to previous directory.
    * Fix leftover paths, and variables from the Linux copy-paste.
    * Update usage note written in the header of the script.
    * Replace ssed in favour of the newer, and better GnuWin32's sed.
    
    Huge thanks to @AndikaBayuS (https://github.com/AndikaBayuS) for the help testing, and debugging the script itself.

--- GovTuner/build.bat
+++ GovTuner/build.bat
@@ -1,14 +1,14 @@
 @Echo Off
+SetLocal EnableDelayedExpansion
 :: Gov-Tuner ZIP creation script
-:: Written by F4uzan, with the help of Gov-Tuner Team
-:: zip and bzip2 binaries provided by GnuWin / gnuwin32 at http://gnuwin32.sourceforge.net
-:: ssed (super sed) provided by http://sed.sourceforge.net/grabbag/ssed/ with win32 binary by Laurent Vogel.
+:: Written by F4, with the help of Gov-Tuner Team
+:: zip, bzip2, and sed binaries provided by GnuWin/gnuwin32 at http://gnuwin32.sourceforge.net
 ::
-:: This script should build a zip archive containing all the files excluding the ".git" folder
-:: "zip" binary is provided in the "win" folder, this folder will also be excluded out when compiling the zip
+:: This script should build a zip archive containing all the files excluding the ".git" folder.
+:: The needed binaries is provided in the "win" folder, this folder will also be excluded out when compiling the zip.
 ::
 :: Usage:
-:: build <ARGUMENTS> <VERSION>
+:: build <VERSION> <TYPE>
 ::
 :: Additional arguments:
 :: build: Build a regular flashable zip
@@ -21,16 +21,17 @@
 
 set version=%1
 set build=%2
-set zip_exec=win\zip.exe
-set sed_exec=win\ssed.exe
-set dir=%cd%
+set home_dir=%cd%
+set prev_dir=%cd%
+set zip_exec=%home_dir%\win\zip.exe
+set sed_exec=%home_dir%\win\sed.exe
 
-if %version% == "" (
+if !version! EQU "" (
 	echo No version number supplied, exiting.
 	exit
 )
 
-if %build% == "" (
+if !build! EQU "" (
 	:: Don't do anything.
 	echo No build type supplied, exiting.
 	exit
@@ -49,43 +50,42 @@ echo:
 echo Building Gov-Tuner v%version%
 echo -----------------------------
 
-if %build% == "build" (
+if !build! EQU build (
 	:: Build and copy uninstaller before doing anything
 	echo Building Uninstaller
 	cd uninstaller
-	..\%zip_dir%\%zip_exec% -r Uninstall_Gov-Tuner.zip .>nul
+	..\%zip_home_dir%\%zip_exec% -r Uninstall_Gov-Tuner.zip .>nul
 	echo Moving Uninstaller to common\system\etc\GovTuner
-	move /y Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner>nul
+	move /y Uninstall_Gov-Tuner.zip ..\common\system\etc\GovTuner>nul
 	cd ..
 	echo Using zip to build output
 	echo Building output zip
-	%zip_exec% -r output/Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "output/*" "magisk/*" "build.*" ".gitignore" "Gov-Tuner_*.zip">nul
-	echo Output created: %dir%\output\Gov-Tuner_%version%.zip
+	%zip_exec% -r output\Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "output/*" "magisk/*" "build.*" ".gitignore" "Gov-Tuner_*.zip">nul
+	echo Output created: %home_dir%\output\Gov-Tuner_%version%.zip
 )
 
-if %build% == "magisk" (
+if !build! EQU magisk (
 	:: Use xcopy instead of robocopy for compatibility reason.
 	echo Copying files
-	xcopy /s /e %dir%\magisk\1500 %dir%\output\temp-magisk
-	xcopy /s /e %dir%\common\system %dir%\output\temp-magisk\system
-	mkdir %dir%\output\temp-magisk\system\bin
-	mkdir %dir%\output\temp-magisk\system\etc\GovTuner\busybox-install
-	mkdir %dir%\output\temp-magisk\system\etc\GovTuner\busybox-install\arm
-	mkdir %dir%\output\temp-magisk\system\etc\GovTuner\busybox-install\x86
-	copy %dir%\arm %dir%\output\temp-magisk\system\etc\GovTuner\busybox-install
-	copy %dir%\x86 %dir%\output\temp-magisk\system\etc\GovTuner\busybox-install
-	copy %dir%\common\system\etc\GovTuner\govtuner %dir%\output\temp-magisk\system\bin\govtuner
-	copy %dir%\common\system\etc\GovTuner\govtuner_hybrid %dir%\output\temp-magisk\system\etc\GovTuner\profiles\govtuner_hybrid
+	xcopy %home_dir%\magisk\1500 %home_dir%\output\temp-magisk /E /V /F /H /N>nul
+	xcopy %home_dir%\common\system %home_dir%\output\temp-magisk\system /E /V /F /H /N>nul
+	mkdir %home_dir%\output\temp-magisk\system\bin
+	mkdir %home_dir%\output\temp-magisk\system\etc\GovTuner\busybox-install
+	mkdir %home_dir%\output\temp-magisk\system\etc\GovTuner\busybox-install\arm
+	mkdir %home_dir%\output\temp-magisk\system\etc\GovTuner\busybox-install\x86
+	copy %home_dir%\arm %home_dir%\output\temp-magisk\system\etc\GovTuner\busybox-install>nul
+	copy %home_dir%\x86 %home_dir%\output\temp-magisk\system\etc\GovTuner\busybox-install>nul
+	copy %home_dir%\common\system\etc\GovTuner\govtuner %home_dir%\output\temp-magisk\system\bin\govtuner>nul
+	copy %home_dir%\common\system\etc\GovTuner\govtuner_hybrid %home_dir%\output\temp-magisk\system\etc\GovTuner\profiles\govtuner_hybrid>nul
 	:: Set the module's version number.
-	%sed_exec% -i -e "s/version=#version/version=v$version-Magisk/g" "$dir/output/temp-magisk/module.prop"
-	%sed_exec% -i -e "s/#version/$version/g" "$dir/output/temp-magisk/config.sh"
+	%sed_exec% -i -e s/version=#version/version=v%version%-Magisk/g %home_dir%\output\temp-magisk\module.prop
+	%sed_exec% -i -e s/#version/%version%/g %home_dir%\output\temp-magisk\config.sh
 	:: Build output.
-	echo Building output zip
-	set prev_dir=%dir%
-	cd %dir\output\temp-magisk
-	%zip_exec% -r ../Gov-Tuner_$version-Magisk.zip . -x "*/\.*">nul
+	cd output\temp-magisk
+	echo Building output zip.
+	%zip_exec% -r ..\Gov-Tuner_%version%-Magisk.zip .>nul
+	pushd %prev_dir%
 	echo Deleting temporary folder
-	cd %prev_dir%
-	rd /s /q %dir%\output\temp-magisk
-	echo Output created: %dir%\output\Gov-Tuner_%version%-Magisk.zip
+	rd /s /q %home_dir%\output\temp-magisk>nul
+	echo Output created: %home_dir%\output\Gov-Tuner_%version%-Magisk.zip
 )
